93ddf933ecaaf64a79a930aed8c7f6f5a01e522b
91093861 <91093861@mvusd.net>
2022-03-18 08:29:31 -0700

    Initial commit

---
+++ src/Book2.java
@@ -0,0 +1,17 @@
+public class Book2
+{
+    public int pages;
+
+    public Book2 (int numsPages)
+    {
+        pages = numsPages;
+    }
+
+    public void pageMessage()
+    {
+        System.out.println("Number of pages: " + pages);
+    }
+}
+
+
+
